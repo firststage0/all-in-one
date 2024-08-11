7fcd5adc0bbdd135ed4bd50a0f9a8fb063ef51ee
firststage0 <red_tk@mail.ru>
2024-08-11 22:18:22 +0300

    Добавлена библиотека firebase для хранения изображений

---
+++ src/functions/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZjf55suU9IzpFvfetjB7TO8Hs0dwZ1WU",
+  authDomain: "all-in-one-test-78946.firebaseapp.com",
+  projectId: "all-in-one-test-78946",
+  storageBucket: "all-in-one-test-78946.appspot.com",
+  messagingSenderId: "1010738405326",
+  appId: "1:1010738405326:web:263096b169112761ea159f",
+};
+
+const app = initializeApp(firebaseConfig);
