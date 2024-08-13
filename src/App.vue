e0467cdb7a8973e43370a7377cdceef984d7d489
firststage <red_tk@mail.ru>
2024-08-13 18:57:51 +0300

    Добавлена библиотека PerfectScrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { onMounted } from "vue";
 import getData from "@/functions/getDataFromServer";
-import jsonData from "@/functions/getDataFromServer";
 
 const url = "https://aiostudy.com/api/v1/courses/get-course?CourseID=3";
 
